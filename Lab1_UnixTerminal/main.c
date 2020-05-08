b9ecab65e13b3ab9907a7458b4efb1656fbb472b
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2020-05-08 15:05:48 -0400

    Fixed formatting issues with the comments

--- Lab1_UnixTerminal/main.c
+++ Lab1_UnixTerminal/main.c
@@ -2,11 +2,10 @@
 //
 //
 #include <stdio.h>
-
 /*
- *  * The power function takes in the base, and the power and calculates the math,
- *   * then returns the result back to the main function to then print.
- *    */
+ * The power function takes in the base, and the power and calculates the math,
+ * then returns the result back to the main function to then print.
+ */
 double power(double base, double n) {
     double result = 1;
     double i = 1;
@@ -16,16 +15,17 @@ double power(double base, double n) {
     }
     return result;
 }
+
 /*
- *  * The main function drives the program. The power function is called here,
- *   * the math is calculated in the power function, and the result is printed here.
- *    * A while loop is used here
- *     */
+ * The main function drives the program. The power function is called here,
+ * the math is calculated in the power function, and the result is printed here.
+ * A while loop is used here.
+ */
 int main () {
-    /*
- *      * Initializing the base, n, result, i for iteration, and maxPower variables
- *           * to pass into the power function.
- *                */
+/*
+ * Initializing the base, n, result, i for iteration, and maxPower variables
+ * to pass into the power function.
+ */
     double base = 2;
     double n = 1;
     double result = 1;
