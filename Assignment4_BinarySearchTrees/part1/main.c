b33887ba9774a0d5a9db134621b9906fd00bc7c4
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2020-06-02 18:47:22 -0400

    Saving

--- Assignment4_BinarySearchTrees/part1/main.c
+++ Assignment4_BinarySearchTrees/part1/main.c
@@ -20,20 +20,44 @@
 unitTest1() {
 	bst_t* empty = create_bst();
 	if (bst_empty(empty) == 1) {
+		bst_print(empty, 1);
 		printf("This tree is empty\n\n");
 
 	} else {
 		printf("Test failed\n\n");
 	}
-	free_bst(empty);
+	
 }
 
+unitTest2() {
+	bst_t* partial = create_bst();
+	bst_add(partial, 1);
+        bst_add(partial, 2);
+	bst_add(partial, 6);
+	bst_add(partial, 4);
+	bst_add(partial, 3);
+	bst_add(partial, 5);
+	if (bst_empty(partial) == 0) {
+                printf("Tree not empty\n\n");
 
+        } else {
+                printf("Test failed\n\n");
+        }
+
+	bst_print(partial, 3);
+	printf("The sum is:%d\n\n", bst_sum(partial));
+	printf("Finding the number %d\n\n", 4, find(partial, 4));
+
+        free_bst(partial);
+
+
+}
 
 
 
 int main(){
 	unitTest1();   
+	unitTest2();
     	return 0;
 }
 
