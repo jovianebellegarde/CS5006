589b2e26c9b91e67ddc9b3cb82f8a9b33743b79c
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2020-06-18 19:57:00 -0400

    Things are working

--- Assignment5_Graphs/main.c
+++ Assignment5_Graphs/main.c
@@ -14,15 +14,34 @@
 // and provide the path to this file which is within
 // our current directory.
 
+void unitTest1() {
+
+	graph_t* partialGraph = create_graph();
+	graph_add_node(partialGraph, 1);
+	graph_add_node(partialGraph, 2);
+	graph_add_node(partialGraph, 3);
+
+	printf("total nodes: 3 == %d\n\n", graph_num_nodes(partialGraph));
+	
+	return 0;
+
+}
+
+
+
+
 int main(int argc, const char * argv[]) {
+    /*
     graph_t * graph = create_graph();
     graph_add_node(graph, 1);
     graph_add_node(graph, 1);
     graph_add_node(graph, 2);
     printf("total nodes: 2 == %d\n", graph_num_nodes(graph));
     return 0;
+    */
 
-
-
+	unitTest1();
+	
+	return 0;
 
 }
